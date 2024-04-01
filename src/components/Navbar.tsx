b3bf9875e13d33ec662b41a484f1ac112d55cc17
Maksym Turlo <maksklas_1@ukr.net>
2024-04-01 19:01:07 +0300

    Raw Navbar mobile adaptive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,17 +12,22 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="w-full flex py-6  bg-purple-800">
+    <nav className="w-full flex py-6 bg-purple-800 navbar">
       <a
         href="/"
-        className="pr-64 pl-12 flex items-center text-gray-200 text-xl"
+        className="mr-64 ml-12 flex items-center text-gray-200 text-xl"
       >
         <img src={logo} alt="enigma" width={60} height={60} />
         Enigma Bank
       </a>
-      <ul className="justify-around list-none sm:flex  hidden items-center flex-1">
+      <ul className="justify-around list-none md:flex  hidden items-center flex-1">
         {navLinks.map((nav, index) => (
-          <li key={nav.id} className={`font-poppins text-lg text-gray-200 `}>
+          <li
+            key={nav.id}
+            className={`font-poppins text-lg text-gray-200 ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
             <a
               className="hover:text-white bg duration-300 hover:shadow-2xl"
               href={`#${nav.id}`}
@@ -32,8 +37,38 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
-      <div className="sm:hidden flex flex-1 justify-end items-center p-12">
-        <button className="" onClick={toggleMenu}>{toggle ? <ImCross className="w-12 h-w-12"/> : <GiHamburgerMenu className="w-12 h-w-12"/>}</button>
+      <div className="md:hidden flex flex-1 justify-end items-center mr-12">
+        <button className="" onClick={toggleMenu}>
+          {toggle ? (
+            <ImCross className="w-6 h-6 text-white" />
+          ) : (
+            <GiHamburgerMenu className="w-6 h-6 text-white" />
+          )}
+        </button>
+      </div>
+      <div
+        className={`${
+          toggle ? "flex" : "hidden"
+        } p-6 bg-purple-950 rounded-lg md:hidden absolute right-0 mx-4 my-2 top-24 min-w-[140px] sidebar`}
+      >
+        <ul className="list-none flex flex-col justify-end items-center">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-gray-200  ${
+                index === navLinks.length - 1 ? "mr-0" : "mb-4"
+              }
+                }`}
+            >
+              <a
+                className="hover:text-white duration-300 hover:shadow-2xl"
+                href={`#${nav.id}`}
+              >
+                {nav.title}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
